a0f1c4eb8463b065c8f3829396fc361680c9e63d
sanjayme049 <sanjayme049@gmail.com>
2022-05-22 10:09:13 +0530

    uploading first demo

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/predict")
+async def root():
+    return {"message": "Hello World"}
+
+   
+@app.get("/predict")
+async def root():
+    return {"message": "Hello india"}
